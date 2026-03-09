c58e61c856cdc06c02262ba031919e81f4120c1c
Davin <davin@example.com>
2026-03-09 09:48:02 +0700

    Initial commit: Project structure and base configuration

---
+++ src/main/java/com/example/pertemuan3/Pertemuan3Application.java
@@ -0,0 +1,13 @@
+package com.example.pertemuan3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pertemuan3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pertemuan3Application.class, args);
+	}
+
+}
